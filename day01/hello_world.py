c14c0c09863faddd1a3252701de972f105072530
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-12-13 19:05:40 -0600

    Create python file

---
+++ day01/hello_world.py
@@ -0,0 +1,14 @@
+print("Hello World") # this is print statement
+print('Hello World') # this sis also print statement
+
+"""
+comment1
+comment2
+comments3
+
+"""
+
+print('Cydeo\nScool')
+print('\tPython Programming')
+print('I love \"Python\"')
+
