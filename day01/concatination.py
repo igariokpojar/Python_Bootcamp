4b8bb89dff990d56d5857d2d5b5973254b734380
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-12-15 08:56:34 -0600

    Python Day 1 variable and concatination

---
+++ day01/concatination.py
@@ -0,0 +1,16 @@
+
+
+name = 'Alex'
+age = 7
+print(' My name is ' + name ) # + IS USE ONLY FOR String concatenation
+
+# print('My age is ' + age) # no concatenation with + when we use an Integer
+
+print('My age is {}'.format(age)) # that is the correct way to concat String with Integer
+
+print('My name is {} and I am {} years old'.format(name,age)) # concatenation String and Integer
+
+print(f'My age is {name} and I am {age} yars old') # place f char b4 you place your String and concatenation is success
+
+print('Python', 3,  'is awsome:', True)
+
---
+++ day01/variable.py
@@ -0,0 +1,18 @@
+"""
+In Java :
+  DataType = Data
+
+  In Python: Dynamic Typing
+     var = Data
+
+"""
+name = 'Alex'
+print(name)
+print(type(name))
+
+age = 40
+print(age)
+
+is_employed = True
+print(type(is_employed))
+
