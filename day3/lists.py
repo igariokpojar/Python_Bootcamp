803cc42d0f401cb3bfa4eed95639c9ebc54a78d2
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2024-01-04 13:57:42 -0600

    lists

--- day3/lists.py
+++ day3/lists.py
@@ -89,4 +89,130 @@ while i < len(nums):  # While loops are not recommend for List Data Structure
 
 print('------------------------------------')
 
+print('------------------------------------')
 
+for i in range(1, 6):
+    i += 2
+    print(i)
+
+print('------------------------------------')
+
+nums = [60, 500, 10, 20, 15, 5, 0]
+
+# nums.sort()  # ascending order
+
+nums.sort(reverse=True)
+
+print(nums)
+
+print('------------------------------------')
+
+list1 = [10, 20, 30, 40]
+
+# list1 = list( reversed(list1) )
+
+list1.reverse()
+
+print(list1)
+
+print('------------------------------------')
+
+tuple_elements = ('Java', 'Python', 'C#', 'Ruby')
+
+list_elements = list(tuple_elements)
+list_elements[-2] = 'C++'
+list_elements.append('SWIFT')
+
+print(list_elements)
+
+tuple_elements = tuple(list_elements)
+
+print(tuple_elements)
+
+print('------------------------------------')
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [1, 2, 3, 4, 5]
+
+print(list1 is list2)
+
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = (1, 2, 3, 4, 5)
+
+print(tuple1 is tuple2)
+
+
+print('------------------------------------')
+
+
+groceries_list = ['Eggs', 'Milk', 'Rice']
+
+groceries_list.append('Chicken')
+groceries_list.extend(('Beef', 'Oranges', 'Onion'))
+
+print(groceries_list)
+
+groceries_list.remove('Beef')
+
+print(groceries_list)
+
+groceries_list.pop()
+
+print(groceries_list)
+
+groceries_list.pop(1)
+
+print(groceries_list)
+
+groceries_list.insert(2, 'Apple')
+
+print(groceries_list)
+
+
+print( groceries_list.index('Eggs'))
+
+nums = [1, 2, 3, 4, 5, 1, 1, 1, 1, 1]
+
+print(nums.count(1))
+
+
+print('--------------- Comprehensions -------------------------')
+
+nums = []
+
+for x in range(1, 51):
+    nums.append(x)
+
+print(nums)
+
+print('------------------------------------')
+
+"""
+divisible_by_5 = []
+
+for x in nums:
+    if x % 5 == 0:
+        divisible_by_5.append(x)
+
+print(divisible_by_5)
+"""
+
+divisible_by_5 = tuple( [ x  for x in nums if x % 5 == 0 ] )
+
+print(divisible_by_5)
+
+print('------------------------------------')
+
+even_nums = [ x for x in nums if x % 2 == 0]
+odd_numbers = [x for x in nums if x % 2 != 0]
+
+print(even_nums)
+print(odd_numbers)
+
+print('------------------------------------')
+
+names = ['Python', 'Java', 'Java', 'JavaScript', 'java', 'JaVA', 'Ruby']
+
+names = [x for x in names if x.lower() != 'java']
+
+print(names)
