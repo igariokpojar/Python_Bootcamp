1e4cb38b7c13c1ffce6f56e7c5b5ca0c42656263
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2024-01-09 09:14:19 -0600

    list_practices2

---
+++ day3/list_practice2.py
@@ -0,0 +1,71 @@
+"""
+Write a program that can remove the duplicated elements from a list
+Ex:
+elements = [1,2,3,4,5,1,2,3,4,5]
+
+Output:
+[1, 2, 3, 4, 5]
+
+Notes: Do Not use Set
+"""
+
+
+def remove_duplicates(input):
+    unique_list = []
+    for e in input:
+        if e not in unique_list:
+            unique_list.append(e)
+    return unique_list
+
+
+elements = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+output = remove_duplicates(elements)
+print(output)
+
+print("-----------------------------------------------")
+"""
+Write a program that can remove string elements from a list if the first and last characters of the string are same
+ex:
+list = {"Anna", "Canada", "Bob", "David", "Lan", "Abida", "Ebrahim", "Farida"}
+output:
+["Lan", "Ebrahim", "Farida"]
+"""
+
+
+def remove_strings_with_same_endpoints(input_list):
+    result_list = [w for w in input_list if w[0] != w[-1]]
+    return result_list
+
+
+in_list = ["Anna", "Canada", "Bob", "David", "Lan", "Abida", "Ebrahim", "Farida"]
+print(remove_strings_with_same_endpoints(in_list))
+
+print('---------------------------------------------')
+
+
+def remove_same_first_last_char(lst):
+    return [word for word in lst if word[0] != word[-1]]
+
+
+lst = ["Anna", "Canada", "Bob", "David", "Lan", "Abida", "Ebrahim", "Farida"]
+print(remove_same_first_last_char(lst))
+
+print('--------------------------------------------')
+
+"""
+Write a program that can display the unique elements of an arrayList:
+ex:
+list = [1, 1, 2, 3, 3, 4, 5, 5]
+output:
+[2, 4]
+"""
+
+
+def unique_elements(input_list):
+    unique = [e for e in input_list if input_list.count(e) == 1]
+    return unique
+
+
+my_list = [1, 1, 2, 3, 3, 4, 5, 5]
+my_result = unique_elements(my_list)
+print(my_result)
