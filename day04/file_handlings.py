cfcd08b50660c8f0ec0e404e5a0429ee10a3c8d1
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2024-02-07 08:50:38 -0600

    reading files

---
+++ day04/file_handlings.py
@@ -0,0 +1,30 @@
+import os
+
+path = 'files/Test.txt'
+
+text_file = open(path, 'r')
+
+print( text_file.read() )
+
+
+text_file.close()
+
+"""
+print(text_file.readline())
+print(text_file.readline())
+print(text_file.readline())
+"""
+
+print('----------------------------------')
+
+path = 'files/Test2.txt'
+
+text_file2= open(path, 'w')
+
+text_file2.write('This is a new text file\nPython created this file')
+
+text_file2.close()
+
+print('----------------------------------')
+
+os.remove(path)
